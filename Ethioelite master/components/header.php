bda46d6cd9589f5cd76b215018670a4bc733cb98
AmmarAdnew <132451928+AmmarAdnew@users.noreply.github.com>
2023-12-01 05:32:30 -0800

    Add files via upload
    Initial commit

---
+++ Ethioelite master/components/header.php
@@ -0,0 +1,15 @@
+<header>
+    <nav>
+        <p class="logo">Ethio<span>Elite</span></p>
+        
+        <div class="nav">
+            <ol>
+                <li href="#"><a href="\Ethioelite master\ethioeli-master\about us.php">About </a></li>
+                <li href="#"><a href="\Ethioelite master\chat boc\chatbox.php">chat</a></li>
+                <li href="#"><a href="\Ethioelite master\ethioeli-master\Contact us.php">Contact </a></li>
+                <li href="#"><a href="\Ethioelite master\resource\Resource.php">resources</a></li>
+            </ol>
+            <button class="btn-login"><a href="\Ethioelite master\ethioeli-master\index.php">Logout</a></button>
+        </div>
+    </nav>
+</header>
